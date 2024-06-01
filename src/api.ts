1337707943a560a3a4b71a432cc017fc982ad097
cristianorneto <cristianorneto@gmail.com>
2024-06-01 14:59:39 -0300

    fix: delete archives unnecessary

--- src/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const api = {
-  async getArtWork() {
-    return axios.get('https://api.artic.edu/api/v1/artworks') // Requisição passará pelo proxy configurado
-  }
-}
--- src/views/Music/MovieDetails.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-containter class=""> oi </v-containter>
-</template>
-
-<script setup lang="ts"></script>
