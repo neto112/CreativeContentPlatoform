08651e390f1218b43c4d82279b9bddee3ac6966c
cristianorneto <cristianorneto@gmail.com>
2024-06-01 15:01:27 -0300

    fix: adjust router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ArtWork from '../views/ArtWork.vue'
+import ArtWork from '../views/ArtWork/ArtWork.vue'
 import ContentDetails from '../views/ContentDetails.vue'
 import GamesView from '../views/Games/GamesView.vue'
 import HomeView from '../views/HomeView.vue'
+import MusicView from '../views/Music/MusicView.vue'
+import VideosView from '../views/Videos/VideosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,8 +28,17 @@ const router = createRouter({
       path: '/artwork',
       name: 'ArtWork',
       component: ArtWork
+    },
+    {
+      path: '/videos',
+      name: 'Videos',
+      component: VideosView
+    },
+    {
+      path: '/music',
+      name: 'Music',
+      component: MusicView
     }
-    //   { path: '/*', redirect: '/' }
   ]
 })
 
