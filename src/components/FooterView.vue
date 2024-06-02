7a4665d642517137d5c6373a0de0fa5852387f8e
cristianorneto <cristianorneto@gmail.com>
2024-06-02 15:07:05 -0300

    feat: add two components Footer and Scrool

---
+++ src/components/FooterView.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer>
+    <v-row justify="center" class="text-center">
+      <v-col cols="12">
+        <span>&copy; {{ new Date().getFullYear() }} Blue Gravity. All rights reserved.</span>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.v-footer {
+  background-color: var(--v-black);
+  color: #fff;
+  padding: 16px;
+}
+</style>
---
+++ src/components/ScrollToTopButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-btn v-if="isVisible" @click="scrollToTop" icon class="scroll-to-top" elevation="2">
+    <v-icon icon="keyboard_arrow_up" />
+  </v-btn>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isVisible = ref(false)
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+const handleScroll = () => {
+  isVisible.value = window.scrollY > 200
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
+
+<style scoped>
+.scroll-to-top {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: var(--v-blue300);
+  color: #fff;
+  opacity: 0.6;
+  z-index: 10;
+  border-radius: 16px;
+}
+</style>
